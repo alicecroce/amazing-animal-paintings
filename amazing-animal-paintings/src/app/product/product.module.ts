8f7ab522e5112500469eea65556e87cb7a6db76d
alicecroce <alicecroce2@gmail.com>
2024-09-04 11:16:49 +0200

    Setting grid system with npm grid-layout

--- amazing-animal-paintings/src/app/product/product.module.ts
+++ amazing-animal-paintings/src/app/product/product.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './product-list/product-list.component';
 import {MatCardModule} from '@angular/material/card';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 
 
@@ -11,7 +12,8 @@ import {MatCardModule} from '@angular/material/card';
   ],
   imports: [
     CommonModule,
-    MatCardModule
+    MatCardModule,
+    FlexLayoutModule
   ]
 })
 export class ProductModule { }
