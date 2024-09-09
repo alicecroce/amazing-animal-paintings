dcaeb2654b79bdd544d29fe4654184b7f54e8b36
alicecroce <alicecroce2@gmail.com>
2024-09-09 09:50:38 +0200

    Checkout section setting: method in the component, HTML configuration and service

--- amazing-animal-paintings/src/app/cart/cart-view/cart-view.component.ts
+++ amazing-animal-paintings/src/app/cart/cart-view/cart-view.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../cart.service';
+import { Product } from 'app/models/product';
 
 @Component({
   selector: 'app-cart-view',
   templateUrl: './cart-view.component.html',
   styleUrl: './cart-view.component.css'
 })
-export class CartViewComponent {
+export class CartViewComponent implements OnInit {
+
+  cartItems:Product[]=[];
+  totalPrice:number=0;
+
+  constructor(private cartService:CartService){
+
+  }
+  ngOnInit(): void {
+    this.cartService.getCartItems().subscribe(data=>{
+      this.cartItems=data;//setto i dati nella card
+      this.totalPrice=this.getTotalPrice();//ed estrapolo la somma totale delle card
+    })
+  }
+
+  getTotalPrice():number{
+    let total=0;
+    for( let item of this.cartItems){
+      total+=item.price;
+  }
+  return total;
+  }
+
+  clearCart():void{
+    this.cartService.clearCart().subscribe();
+}
+
+checkout():void{
+
+}
 
 }
--- amazing-animal-paintings/src/app/cart/cart.service.ts
+++ amazing-animal-paintings/src/app/cart/cart.service.ts
@@ -9,22 +9,29 @@ import { Product } from 'app/models/product';
 })
 export class CartService {
 
-  private apiUrl=environment.apiUrl+"/cart";
+  private apiCartUrl=environment.apiUrl+"/cart";//api per il carrello
+  private apiCheckoutUrl=environment.apiUrl+"/checkout";//api per il checkout
+
 
   constructor(private http:HttpClient) { }
 
   //METODO PER AGGIUNGERE AL CARRELLO
   addToCart(product:Product):Observable<Product>{
-    return this.http.post<Product>(this.apiUrl,product);
+    return this.http.post<Product>(this.apiCartUrl,product);
   }
 
   //METODO PER VEDERE GLI ELEMENTI AGGIUNTI NEL CARRELLO
   getCartItems():Observable<Product[]>{
-  return this.http.get<Product[]>(this.apiUrl);
+  return this.http.get<Product[]>(this.apiCartUrl);
   }
 
   //METODO PER CANCELLARE UN ELEMENTO DAL CARRELLO(richiesta di cancellazione al server)
   clearCart():Observable<void>{
-    return this.http.delete<void>(this.apiUrl);
+    return this.http.delete<void>(this.apiCartUrl);
   }
+
+  //METODO PER IL CHECKOUT DEI PRODOTTI (QUINDI è UN POST, CHE HO CREATO SU MOCKOON)
+checkout(products: Product[]):Observable<void>{
+  return this.http.post<void>(this.apiCheckoutUrl, products);//stiamo inviando materiale, ma non riceviamo nulla in cambio (void)
+}
 }
